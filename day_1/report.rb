086e9440766373083083c964bdced9f4b2e9383d
denissellu <hingasellu@gmail.com>
2020-12-02 00:18:44 +0000

    Refactor to allow for task 2

--- day_1/report.rb
+++ day_1/report.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-def get_2020_expense(file_path)
+def get_2020_expense(file_path, number_of_expenses)
   expenses = []
 
   File.open(file_path, 'r').each do |line|
     expenses << line.to_i
   end
 
-  expenses.select { |e| expenses.include?(2020 - e) }.inject(:*)
+  expenses.combination(number_of_expenses).to_a.select{|a| a.sum == 2020 }.flatten.inject(:*)
 end
 
 pp get_2020_expense('./input.txt')
--- day_1/spec/report_spec.rb
+++ day_1/spec/report_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'Daily Report' do
   let(:file_path) { 'spec/fixtures/files/input.txt' }
 
   it 'returns the sum of the two value that eq 2020' do
-    expect(get_2020_expense(file_path)).to eq 514_579
+    expect(get_2020_expense(file_path, 2)).to eq 514_579
+  end
+
+  it 'returns the sum of the three value that eq 2020' do
+    expect(get_2020_expense(file_path, 3)).to eq  241_861_950
   end
 end
