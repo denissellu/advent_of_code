47d4148b864e9b8206613fc5b1eee51d34d493ff
denissellu <hingasellu@gmail.com>
2020-12-03 00:23:25 +0000

    Add soultion for day 2

--- day_2/main.rb
+++ day_2/main.rb
@@ -18,3 +18,26 @@ def valid_passwords(file_path)
 end
 
 pp valid_passwords('./input.txt')
+
+
+#####################################
+# TASK 2
+# ###################################
+
+def index_valid_passwords(file_path)
+  passwords = []
+
+  File.open(file_path, 'r').each do |line|
+    passwords << line.strip.split
+  end
+
+  passwords.select do |p|
+    min, max = p[0].split('-').map(&:to_i)
+    check_char = p[1][0]
+    password = p[2]
+
+    [password[min-1] == check_char, password[max-1] == check_char].count(true) == 1
+  end.count
+end
+
+pp index_valid_passwords('./input.txt')
--- day_2/spec/main_spec.rb
+++ day_2/spec/main_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe 'Password Philosophy' do
   let(:file_path) { 'spec/fixtures/files/input.txt' }
 
   it 'get the valid number of password' do
-    expect(valid_password(file_path)).to eq 2
+    expect(valid_passwords(file_path)).to eq 2
+  end
+
+  it 'get the valid number of password' do
+    expect(index_valid_passwords(file_path)).to eq 1
   end
 end
